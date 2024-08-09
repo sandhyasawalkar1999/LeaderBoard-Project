068df831f9bb1e9eae0a2eafeb48d8ef87cadd27
sandhyasawalkar1999 <sandhyasawalkar1999@gmail.com>
2024-08-09 16:02:17 +0530

    project is updated

--- index.js
+++ index.js
@@ -1,106 +1,145 @@
-console.log("hello")
+console.log("hello");
 let fname = document.getElementById("first name");
 let lname = document.getElementById("last name");
 let contry = document.getElementById("contry");
 let score = document.getElementById("score");
 let addScore = document.getElementById("add-score");
 let scoreReport = document.getElementById("score-report");
-console.log(scoreReport)
+console.log(scoreReport);
 
-let playerList =[];
+let playerList = [];
 
-addScore.addEventListener("click" ,function(e) 
-{
-    e.preventDefault();
-    if(fname == "" || lname == "" || contry == "" || score == "" ){
-        
-        alert("please enter All information !!!!");
-    }
+addScore.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (fname == "" || lname == "" || contry == "" || score == "") {
+    alert("please enter All information !!!!");
+  } else {
+    let mDate = new Date();
+    // console.log(mDate);
+    let date = mDate.getDate();
+    let month = mDate.getMonth() + 1;
+    let year = mDate.getFullYear();
 
-    else{
-         
-        let mDate = new Date();
-        // console.log(mDate);
-        let date = mDate.getDate();
-        let month = mDate.getMonth()+1;
-        let year = mDate.getFullYear();
-        
-        let player ={
-            fname:`${fname.value} `,
-            lname :`${lname.value}`,
-        contry:`${contry.value}`,
-        score : `${score.value}`,
-        date:`${date}-${month}-${year}`
-       }
-       
+    let player = {
+        id: Date.now(),
+      //   id: `player-${playerList.length + 1}`,  // for unique id, but not required for this task.
+      fname: `${fname.value} `,
+      lname: `${lname.value}`,
+      contry: `${contry.value}`,
+      score: `${score.value}`,
+      date: `${date}-${month}-${year}`,
+    };
+
     playerList.push(player);
-    
+
     updateData(playerList);
-    fname.value="";
-    lname.value ="";
-    contry.value ="";
+    fname.value = "";
+    lname.value = "";
+    contry.value = "";
     score.value = "";
-   }
+  }
 });
 
 console.log(playerList);
 
-function updateData(){
-
-    // if(playerList.length > 0){
-    //     // scoreReport.style.display="block";
-    //     // scoreReport.style.display="block";
-    // }
-    // else {
-    //         // scoreReport.style.display ="none";
-    // }
-
-    scoreReport.innerHTML= "";
-    playerList.sort((player1,player2 ) => player2.score - player1.score);
-
-    playerList.forEach((items,index) =>{
-        let main =document.createElement("div");
-        main.setAttribute("data-index", index);
-        let fname = document.createElement("div");
-        fname.textContent = items.fname;
-        let lname = document.createElement("div");
-        lname.textContent = items.lname;
-        let contry= document.createElement("div");
-        contry.textContent = items.contry;
-        let score = document.createElement("div");
-        score.textContent = items.score;
-        let del = document.createElement("button");
-        del.textContent = "delete";
-        del.id = "deletebtn";
-        let inc = document.createElement("button");
-        inc.textContent ="increase";
-        inc.id ="incbtn";
-        let dec = document.createElement("button");
-        dec.textContent = "decrese";
-        dec.id = "decbtn";
-
-        main.appendChild(fname);
-        main.appendChild(lname);
-        main.appendChild(contry);
-        main.appendChild(score);
-        main.appendChild(del);
-        main.appendChild(inc);
-        main.appendChild(dec);
-
-        document.body.appendChild(main);
-        document.getElementById("deletebtn").addEventListener("click",handleDelete);
-
-    })
-}
-
-function handleDelete(){
+function updateData() {
+    console.log(playerList);
     
-    let indexValue = this.parentElement.getAttribute("data-index");
-    playerList.splice(indexValue,1);
-    this.parentElement.remove();
-    updateData();
-}
-let incbtn = document.getElementById("incbtn");
+  if(playerList.length > 0){
+      scoreReport.style.display="block";
+      // scoreReport.style.display="block";
+  }
+  else {
+          scoreReport.style.display ="none";
+  }
 
-let decbtn = document.getElementById("decbtn");
+  scoreReport.innerHTML = "";
+  playerList.sort((player1, player2) => player2.score - player1.score);
+
+  playerList.forEach((items, index) => {
+    console.log(items.score);
+    
+    let main = document.createElement("div");
+    main.setAttribute("data-index", index);
+    main.id = "maindiv";
+
+    let fname = document.createElement("div");
+    fname.textContent = items.fname;
+    fname.id = "fname";
+
+    let lname = document.createElement("div");
+    lname.textContent = items.lname;
+    lname.id = "lname";
+
+    let contry = document.createElement("div");
+    contry.textContent = items.contry;
+    contry.id = "contry";
+
+    let score = document.createElement("div");
+    score.textContent = items.score;
+    score.id = "score";
+
+    let inc = document.createElement("button");
+    inc.dataId = items.id;
+    inc.textContent = "+5";
+    inc.id = "incbtn";
+
+    inc.addEventListener("click",function(e){
+        console.log(e.target.dataId);
+        playerList.forEach((player)=>{
+            if(player.id == e.target.dataId){
+                // player.score += 5;
+                player.score = parseInt(player.score) + 5; 
+            }
+        })        
+        // let score = parseInt(score);
+        // score += 5;
+        // score.value = score;
+        updateData();
+    })
+    let dec = document.createElement("button");
+    dec.dataId = items.id;
+    dec.textContent = "-5";
+    dec.id = "decbtn";
+
+
+    dec.addEventListener("click" ,function(e){
+        console.log(e.target.dataId);
+        playerList.forEach((player)=>{
+            if(player.id == e.target.dataId){
+                player.score = parseInt(player.score) - 5; 
+            }
+        })        
+        // let score = parseInt(score);
+        // score += 5;
+        // score.value = score;
+        updateData();
+    })
+
+    let del = document.createElement("button");
+    del.textContent = "delete";
+    del.id = "deletebtn";
+
+
+    main.appendChild(fname);
+    main.appendChild(lname);
+    main.appendChild(contry);
+    main.appendChild(score);
+    main.appendChild(inc);
+    main.appendChild(dec);
+    main.appendChild(del);
+
+    document.getElementById("score-report").appendChild(main);
+    document
+      .getElementById("deletebtn")
+      .addEventListener("click", handleDelete);
+  });
+}
+
+function handleDelete() {
+  let indexValue = this.parentElement.getAttribute("data-index");
+  playerList.splice(indexValue, 1);
+  this.parentElement.remove();
+  updateData();
+}
 
